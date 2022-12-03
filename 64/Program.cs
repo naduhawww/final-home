f288b3f2b3559a3435cdb0839170329017c44440
naduhawww <117995427+naduhawww@users.noreply.github.com>
2022-12-03 22:29:42 +0700

    Add files via upload

---
+++ 64/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+// (с помощью рекурсии)
+//  453 -> 12
+//  45 -> 9
+
+Console.WriteLine("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+int RecursionSum(int a)
+{
+    if (a < 1) return 0;
+    return RecursionSum(a / 10) + a % 10;
+}
+
+Console.WriteLine($"Сумма числа {a} равна {RecursionSum(a)}");
---
+++ 66/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму 
+// натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.WriteLine("Введите первое число: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int Sum(int M, int N)
+{
+    if (N == M) return N;
+    return M + Sum(M + 1, N);
+}
+Console.WriteLine($"Сумма элементов в промежутке между {M} и {N} равна {Sum(M,N)} ");
